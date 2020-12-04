4cdde4bcd9074d06bc9501326060e51e93692e8c
Tristan Miller <tem83@nau.edu>
2020-12-04 16:44:57 -0700

    Converted from using files to file paths

--- PeakSegDisk/SegDisk.py
+++ PeakSegDisk/SegDisk.py
@@ -2,5 +2,5 @@ import PeakSegDiskInterface
 
 
 def FPOP_files(coverage, segments, loss, penalty, db):
-    return PeakSegDiskInterface.interface(coverage.name, segments.name, loss.name, penalty, db.name)
+    return PeakSegDiskInterface.interface(coverage, segments, loss, penalty, db)
 
--- tests/test_PeakSegDisk.py
+++ tests/test_PeakSegDisk.py
@@ -7,18 +7,15 @@ dataPath = os.path.join('data')
 
 def test_PeakSegDisk():
     coveragePath = os.path.join(dataPath, 'coverage.bedGraph')
-    coverageFile = open(coveragePath, 'r')
     segmentsPath = os.path.join(dataPath, 'segments.bed')
     correctSegmentsPath = os.path.join(dataPath, 'correctSegments.bed')
-    segmentsFile = open(segmentsPath, 'w')
     lossPath = os.path.join(dataPath, 'loss.tsv')
     correctLossPath = os.path.join(dataPath, 'correctLoss.tsv')
-    lossFile = open(lossPath, 'w')
 
     penalty = '100000'
     dbFile = tempfile.NamedTemporaryFile(suffix='.db')
 
-    correct = PeakSegDisk.FPOP_files(coverageFile, segmentsFile, lossFile, penalty, dbFile)
+    correct = PeakSegDisk.FPOP_files(coveragePath, segmentsPath, lossPath, penalty, dbFile.name)
 
     assert correct
 
